43b40f0598dc657dcc54834dfec7339e5ab92459
cterech <cterech@gmail.com>
2024-01-23 12:29:06 -0500

    remove hard coded offer params

--- src/createOffer.ts
+++ src/createOffer.ts
@@ -3,9 +3,9 @@ import { WALLET_ADDRESS, sdk} from './utils/constants';
 const createOffer = async () => {
     
     // TODO: Fill in the token address and token ID of the NFT you want to make an offer on
-    let tokenAddress: string = "0x26acca8836164119fb4dd0d31917627c68558ec9";
-    let tokenId: string = "6546";
-    let offerAmount: string = "0.004";
+    let tokenAddress: string = "";
+    let tokenId: string = "";
+    let offerAmount: string = "";
 
     const offer = {
         accountAddress: WALLET_ADDRESS,
